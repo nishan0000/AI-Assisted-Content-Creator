908362399549538f8783a5f6c1607afe322538da
nishan0000 <mdnishan006@gmail.com>
2024-02-25 10:42:23 +0530

    Requirement file added and bugs fixed

--- PyFiles/app.py
+++ PyFiles/app.py
@@ -5,6 +5,7 @@ import configs as cf
 
 app = Flask(__name__)
 
+
 @app.route('/process', methods=['POST'])
 def create_reel():
 
@@ -12,49 +13,21 @@ def create_reel():
         data = request.get_json()  # Get JSON data sent in the POST request
         tweet_link = data.get('url', '')
         text = data.get('text', '')
-
-        if text == None:
-            # Generating hookline using AI (Gemini API)
-            text, caption = TextGen.generate_text_using_ai(tweet_link)
-        else:
-            pass
-
-        # Creating project_id
-        project_id = AIReelGen.create_project()
-
-        # Creating project
-        project_creation_status = AIReelGen.create_project_folder(project_id)
-
-        # Downloading and saving the video to specified directory (This should be changed with download code later)
-        video_download_status = AIReelGen.download_twitter_video(tweet_link, project_id)
-
-        # Generating initial video with small banner added to left and right : 14px
-        w_gen_status = AIReelGen.generate_video_w(project_id)
-
-        # Generating the image with text added
-        img_gen_status = AIReelGen.generate_header_image(text, project_id)
-
-        # Combining the image and video together
-        combine_status = AIReelGen.combine_img_vid(project_id)
-
-        # Generating Reel
-        reel_gen_status = AIReelGen.generate_final_video(project_id)
-
-        # Adding audio to the created video
-        audio_video_status = AIReelGen.add_audio_to_final_video(project_id)
         
-        # Clearing the directory
-        AIReelGen.clear_directory(project_id)
-
-        # Success status
-        completion_status = 'Completed'
+        # Running the function
+        caption, completion_status, project_id = AIReelGen.create_reel(tweet_link, text)
+        
+        # What to return
+        rets = jsonify({'Status': completion_status, 'Caption' : caption, 'Project ID' : project_id})
+        
+        return rets
 
     except Exception as e:
         
-        # Failure Status
-        completion_status = 'Failed'
+        # What to return
+        rets = jsonify({'Status': completion_status, 'Project ID' : project_id})
 
-    return jsonify({'Status': completion_status, 'Caption' : caption, 'Project ID' : project_id})
+        return rets
 
 
 @app.route('/download', methods=['GET'])
